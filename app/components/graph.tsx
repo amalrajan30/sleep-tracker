caae427aea2b207d32503d3efeecc368da1a063d
Amal <amalrajan30@gmail.com>
2021-12-10 11:39:36 +0530

    use data from prop

--- app/components/graph.tsx
+++ app/components/graph.tsx
@@ -1,30 +1,11 @@
-import * as React from 'react'
 import { AreaChart, Area, Tooltip, XAxis, YAxis, ResponsiveContainer } from 'recharts'
 
-type DailySleep = {
+interface DailySleep {
   date: string
   hours: number
 }
 
-type SleepData = {
-  label: string
-  data: DailySleep[]
-}
-
-export default function Graph() {
-  const data = React.useMemo(
-    (): DailySleep[] => [
-      { date: '01/11', hours: 2 },
-      { date: '02/11', hours: 5 },
-      { date: '03/11', hours: 7 },
-      { date: '04/11', hours: 8 },
-      { date: '05/11', hours: 6 },
-      { date: '06/11', hours: 8 },
-      { date: '07/11', hours: 4 },
-      { date: '08/11', hours: 5 },
-    ],
-    []
-  )
+export default function Graph({ data }: { data: DailySleep[] }) {
 
   return (
     <div className="w-full mt-4" style={{ height: 'calc(100% - 22px - 1rem)' }}>
