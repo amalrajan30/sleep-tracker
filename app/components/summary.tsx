de49df8fa954dd30d40cef6427065606ccba1042
Amal <amalrajan30@gmail.com>
2021-12-10 11:42:00 +0530

    use data from prop

--- app/components/summary.tsx
+++ app/components/summary.tsx
@@ -1,16 +1,24 @@
-export default function Summary() {
+interface Summary {
+  avgSleepTime: string
+  avgWakeTime: string
+  avgSleepDuration: number
+  daysLessThan6Hours: number
+  daysMoreThan8Hours: number
+}
+
+export default function Summary({ data }: { data: Summary }) {
   return (
     <div className="grid gap-2 mt-4 text-gray-500" style={{ gridTemplateColumns: '80% auto' }}>
       <p className="font-semibold inline-block">Avg sleep time</p>
-      <p className="font-bold">22:00</p>
+      <p className="font-bold">{data.avgSleepTime}</p>
       <p className="font-semibold inline-block">Avg wake up time</p>
-      <p className="font-bold">6:00</p>
+      <p className="font-bold">{data.avgWakeTime}</p>
       <p className="font-semibold inline-block">Avg sleep duration</p>
-      <p className="font-bold">6hrs</p>
+      <p className="font-bold">{data.avgSleepDuration ? `${data.avgSleepDuration} hrs` : 0}</p>
       <p className="font-semibold inline-block">No of days slept less than 6hrs </p>
-      <p className="font-bold">6days</p>
+      <p className="font-bold">{data.daysLessThan6Hours}</p>
       <p className="font-semibold inline-block">No of days slept more than 8hrs </p>
-      <p className="font-bold">6days</p>
+      <p className="font-bold">{data.daysMoreThan8Hours}</p>
     </div>
   )
 }
