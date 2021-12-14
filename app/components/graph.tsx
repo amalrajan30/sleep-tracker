1eb5654c8762c0a92d6ab9a6e23e2885cbc173e3
Amal <amalrajan30@gmail.com>
2021-12-14 13:09:01 +0530

    Minor style fix

--- app/components/graph.tsx
+++ app/components/graph.tsx
@@ -6,13 +6,12 @@ interface DailySleep {
 }
 
 export default function Graph({ data }: { data: DailySleep[] }) {
-
   return (
     <div className="w-full mt-4" style={{ height: 'calc(100% - 22px - 1rem)' }}>
       <ResponsiveContainer width="100%">
-        <AreaChart data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
-          <Area type="monotone" dataKey="hours" stroke="#8884d8" />
-          <XAxis dataKey="date" />
+        <AreaChart data={data} margin={{ top: 5, right: 0, left: -36, bottom: 0 }}>
+          <Area type="monotone" dataKey="hours" stroke="#8884d8" fill="#8884d8" />
+          <XAxis dataKey="date" fill="rgb(229 231 235)" />
           <YAxis />
           <Tooltip />
         </AreaChart>
